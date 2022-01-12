0964b524358f5c705ff9bf2520ba079e6640636e
mdbasoglu <69213997+mdbasoglu@users.noreply.github.com>
2022-01-12 18:20:39 +0100

    neue Entwicklungen

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -7,47 +7,76 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
-
-            Random rand = new Random();
-
-            string[] worte = File.ReadAllLines("/Users/macbook/Documents/GitHub/Hangman/Hangman/Wortliste.txt");
-
-            int index = rand.Next(worte.Length);
-            
-            string zufallwort = worte[index];
-
-            for (int i = 0; i < zufallwort.Length ; i++)
-            {
-                Console.Write("_");
-                
-            }
-            Console.WriteLine();
-
-
-
-
-            /*
-            for (int i = 0; i < worte.Length; i++) //Das Lesen jeder Zeilen von txt File
-            {
-                Console.WriteLine(worte[i] + ' ');    
-            }
-            */
-
-
             Console.WriteLine("Hangman - Finde das geheime Wort \n");
-            Console.WriteLine("");
+
+            //kaynak, rastgele secim, atama
+            string[] woerterliste = File.ReadAllLines("/Users/macbook/Documents/GitHub/Hangman/Wortliste.txt");
+            Random zufallzahlen = new Random();
+            string gewaehtewort = woerterliste[zufallzahlen.Next(0, woerterliste.Length)];
+            //Console.WriteLine(secilenkelime); rastgele kelimeyi gösterir
 
 
+            //secilen kelime kadar _ olusturma
+            char[] charakter = new char[gewaehtewort.Length];
+            for (int i = 0; i < charakter.Length; i++)
+            {
+                charakter[i] = '_';
+            }
 
+            //Asil isleri olusturacagimiz döngü
+            do
+            {
+                bool kol = false;
 
+                //karakteri ekrana yazdirma
+                foreach (var item in charakter)
+                {
+                    Console.Write(item + " ");
+                }
+
+                //ekrandan girdi isteme
+                Console.WriteLine();
+                Console.Write("Bitte ein Buchstaben eingeben : ");
+
+                char buchstaben = char.Parse(Console.ReadLine());  //char harf = char.Parse(Console.ReadLine().ToUpper()); kücük veya büyük harf farketmez to upper ile
+
+                //Girilen harf ile secilen kelimeyi kontrol etmek ve karakterin elemani ile degistirmek
+                for (int i = 0; i < gewaehtewort.Length; i++)
+                {
+                    if (buchstaben == gewaehtewort[i])
+                    {
+                        charakter[i] = buchstaben;
+                        kol = true;
+                    }
+                }
+
+                if (buchstaben >= 'a' && buchstaben <= 'z')
+                {
+                    Console.WriteLine("Bitte Großbuchstabe eingeben : " + buchstaben);
+                    Console.WriteLine("ungültige Eingabe");
+                    Console.WriteLine();
+                    if (gewaehtewort.Length == charakter.Length)
+                    {
+                        Console.WriteLine("Mit "+ ""+"Fehlerversuchen gefunden");
+                    }
+                }
+
+                
+
+                /*
+                //Wenn wir Punkte geben möchten
+                if (kol == true)
+                {
+                    punkte += 30;
+                }
+                else
+                {
+                    punkte -= 30;
+                }
+                Console.WriteLine("Punkte Status" + punkte);
+                */
+            } while (true);
+            Console.ReadKey();
         }
-
-
-
-        public static void wortliste()
-        {
-        }
-
-
     }
 }
